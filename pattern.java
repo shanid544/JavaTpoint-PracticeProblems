99f2e143dcb7f3eff685a9960a4490cb8bb4d2ce
shanid544 <shanidvpputhanpeedika@gmail.com>
2022-07-25 12:57:03 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        int i,j;
+        for(i=1;i<7;i++){
+            for(j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
