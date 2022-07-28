88de70adad5bdfd7d04d65c23b14ec7aeb9f7b26
shanid544 <shanidvpputhanpeedika@gmail.com>
2022-07-28 10:05:38 +0530

    Create CreateObj2Example.java

---
+++ CreateObj2Example.java
@@ -0,0 +1,13 @@
+class CreateObj2Ex {
+    CreateObj2Ex(){
+        System.out.println("hiiiiiiiiiiiiiiiii");
+    }
+}
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        CreateObj2Ex obj = new CreateObj2Ex();
+        
+    }
+}
