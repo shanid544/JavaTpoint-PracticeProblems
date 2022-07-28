107013c0ae1ec17c6a4352428c77d0aca788849d
shanid544 <shanidvpputhanpeedika@gmail.com>
2022-07-28 10:03:41 +0530

    Create CreateObj1Example.java

---
+++ CreateObj1Example.java
@@ -0,0 +1,17 @@
+//How to Create Object in Java
+
+//https://www.javatpoint.com/how-to-create-object-in-java
+
+class CreateObj1Ex {
+    public void show(){
+        System.out.println("hiiiiiiiiiiiiiiiii");
+    }
+}
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        CreateObj1Ex obj = new CreateObj1Ex();
+        obj.show();
+    }
+}
