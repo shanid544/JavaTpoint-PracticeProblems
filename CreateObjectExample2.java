cc58d412324fa9124f39cef6bdfdc24a9429fff6
shanid544 <shanidvpputhanpeedika@gmail.com>
2022-08-06 08:36:52 +0530

    Create CreateObjectExample2.java

---
+++ CreateObjectExample2.java
@@ -0,0 +1,14 @@
+//By using the new keyword, we can also invoke the constructor (default or parametrized) of the class.
+
+public class CreateObjectExample2{
+
+	CreateObjectExample2(){
+	
+		System.out.println("shanid");
+	}
+	
+	public static void main(String[] args){
+	
+		CreateObjectExample2 obj = new CreateObjectExample2();
+	}
+}
